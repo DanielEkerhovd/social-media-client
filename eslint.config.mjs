c15378f213fd42d4c6118765ef3727f0bb27a241
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-08-15 20:13:52 +0200

    Updated eslint with setting for cypress and jest, and updated the mjs file with npx @eslint/migrate-config .eslintrc.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,6 @@
 import globals from "globals";
+import cypress from "eslint-plugin-cypress";
+import jest from "eslint-plugin-jest";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
@@ -23,4 +25,43 @@ export default [...compat.extends("eslint:recommended"), {
     },
 
     rules: {},
+}, ...compat.extends("plugin:cypress/recommended").map(config => ({
+    ...config,
+    files: ["**/*.cy.js"],
+})), {
+    files: ["**/*.cy.js"],
+
+    plugins: {
+        cypress,
+    },
+
+    languageOptions: {
+        globals: {
+            ...cypress.environments.globals.globals,
+        },
+    },
+
+    rules: {
+        "cypress/no-unnecessary-waiting": "off",
+        "no-unused-vars": "off",
+    },
+}, ...compat.extends("plugin:jest/recommended").map(config => ({
+    ...config,
+    files: ["**/*.test.js"],
+})), {
+    files: ["**/*.test.js"],
+
+    plugins: {
+        jest,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.jest,
+        },
+    },
+
+    rules: {
+        "jest/prefer-expect-assertions": "off",
+    },
 }];
