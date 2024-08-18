d1a1138db164bcf705c76b1ffa98ef627ac37f0e
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-08-18 15:03:22 +0200

    added .env dependency for cypress .env functionality, if needed

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const dotenv = require('cypress-dotenv');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      // Load .env variables into Cypress environment
+      config = dotenv(config);
+
+      // You can add other event listeners here if needed
+
+      // Return the modified config
+      return config;
+    }
   },
 });
