45ed4655495325954f24f9339b10466716485ec0
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-08-18 15:47:57 +0200

    Fixed the logout unit test, was missing a token

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -4,6 +4,9 @@ import { logout } from './logout.js';
 
 global.localStorage = {
     store: {},
+    setItem: jest.fn(function (key, value) {
+        this.store[key] = value.toString();
+    }),
     getItem: jest.fn(function (key) {
         return this.store[key];
     }),
@@ -15,6 +18,8 @@ global.localStorage = {
 describe("logout function", () => {
 
     it("should clear the token from browser storage", () => {
+
+        localStorage.setItem("token", "dummyToken");
         
         logout();
 
